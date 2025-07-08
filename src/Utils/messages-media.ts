6033d76d1f8d572dd5e1af75836da271691ce892
Angela <octocat@github.com>
2025-07-08 10:54:22 +0700

    fix: image processing lib

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -137,7 +137,7 @@ export const extractImageThumb = async (bufferOrFilePath: Readable | Buffer | st
 	}
 
 	const lib = await getImageProcessingLibrary()
-	if ('jimp' in lib && typeof lib.jimp === 'object' && (typeof lib.jimp?.Jimp === 'object' || typeof jimp?.Jimp === 'function')) {
+	if ('jimp' in lib && typeof lib.jimp === 'object' && (typeof lib.jimp?.Jimp === 'object' || typeof lib.jimp?.Jimp === 'function')) {
 		const jimp = (lib.jimp?.default?.Jimp?.read || lib.jimp?.Jimp?.read);
 		console.log("Jimp Read:", jimp)
 	}
